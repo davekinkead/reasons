1fc28217d2294b5cda3c7f7d7ee1c55eba62c6e4
Dave Kinkead <dave@kinkead.com.au>
2017-04-26 09:02:15 +1000

    Fix typo in highlighter URL

--- reasons.js
+++ reasons.js
@@ -380,7 +380,7 @@ function isNode (el) {
 }
 },{"array-difference":14,"array-flatten":16,"array-intersection":17,"array-unique":20}],3:[function(require,module,exports){
 const Utils = require('./utils')
-const MAP_URL = 'http::/dave.kinkead.com.au/reasons'
+const MAP_URL = 'http://dave.kinkead.com.au/reasons'
 const reasons = []
 
 module.exports = Highlighter
@@ -595,8 +595,8 @@ Reason.prototype.collides = function(el) {
   }
 }
 },{}],6:[function(require,module,exports){
-//  Reasons.js
-//  Copyright (c) 2017 Dave Kinkead
+//  Reasons.js by Dave Kinkead
+//  Copyright (c) 2017 University of Queensland
 //  Available under the MIT license
 
 //  Reasons.js API
