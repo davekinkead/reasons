27191ff6ccd43442a1eb1d74347ceba929caaec8
Dave Kinkead <dave@kinkead.com.au>
2017-05-24 09:57:05 +1000

    Stash

--- ROADMAP.md
+++ ROADMAP.md
@@ -2,13 +2,16 @@ Feature Roadmap
 
 0.1.0
 
+  - [] fix indepdendent conjoined arguments
   - [] canvas events should update graph object
+  - [] autofocus reason text on edit
   - [] hover over relation text to tigger edit
   - [] zoom map
   - [] render map from x,y coordinates
+  
+  - [] replace premise/conclusion with 'save'
   - [] display already highlighted text
   - [] delete already highlight text
-  - [] replace premise/conclusion with 'save'
 
 
 Tests
--- test/graph.js
+++ test/graph.js
@@ -18,18 +18,16 @@ const map = {
     f: {id: 'f', text: 'blah blah f'},
     g: {id: 'g', text: 'blah blah g'},
     h: {id: 'h', text: 'blah blah h'},
-    i: {id: 'i', from: 'a', to: 'b'}, 
-    j: {id: 'j', from: ['b', 'c'], to: 'd'}, 
-    k: {id: 'k', from: 'f', to: 'g'},
-    l: {id: 'l', from: 'h', to: 'd'},
-    m: {id: 'm', from: 'b', to: 'f'}
+    i: {id: 'i', text: 'blah blah i'},
+    j: {id: 'j', text: 'blah blah j'},
+    k: {id: 'k', text: 'blah blah k'},
+    l: {id: 'l', text: 'blah blah l'},
+    ab: {id: 'ab', from: 'a', to: 'b'}, 
+    ac: {id: 'ac', from: 'a', to: 'c'}, 
+    bc: {id: 'bc', from: 'b', to: 'c'}, 
+    ca: {id: 'ca', from: 'c', to: 'a'}, 
+    cd: {id: 'cd', from: 'c', to: 'd'}, 
   }
-
-const elements = []
-for (key in map) {
-  elements.push(map[key])
-}
-const graph = new Graph(elements)
 
 describe('Graph', () => {
   describe('#new', () => {
@@ -38,52 +36,87 @@ describe('Graph', () => {
     })
 
     it('should accept an array of objects', () => {
-      new Graph(elements)
+      new Graph([map.a, map.b])
     })
 
     it('should be an instance of Array', () => {
-      new Graph(elements).should.be.instanceof(Array)
+      new Graph([map.a, map.b]).should.be.instanceof(Array)
+    })
+  })
+
+  describe('#add', () => {
+    it('should add a new node to the graph', () => {
+      const graph = new Graph([map.a, map.b, map.c, map.ac, map.bc])
+      graph.add(map.d)
+      graph.nodes().length.should.equal(4)
+    })
+
+    it('should add a new edge to the graph', () => {
+      const graph = new Graph([map.a, map.b, map.c, map.ac, map.bc])
+      graph.add(map.cd)
+      graph.edges().length.should.equal(3)
+    })
+
+    it('should add circular edge to the graph', () => {
+      const graph = new Graph([map.a, map.b, map.c])
+      graph.add(map.ab)
+      graph.add(map.bc)
+      graph.add(map.ca)
+      graph.edges().should.have.length(3)
+    })    
+
+    it('should add conjoined edge to the graph', () => {
+      const graph = new Graph([map.a, map.b, map.c, map.ac, map.bc])
+      graph.add(map.ab)
+      graph.edges().should.equal(1)
     })
   })
 
   describe('#edges', () => {
     it('should return all the edges', () => {
+      const graph = new Graph([map.a, map.b, map.c, map.ac, map.bc])
       graph.edges().should.be.instanceof(Array)
-      graph.edges().should.have.length(5)
+      graph.edges().should.have.length(2)
     })
   })
 
   describe('#nodes', () => {
     it('should return all the nodes', () => {
+      const graph = new Graph([map.a, map.b, map.c, map.ac, map.bc])
       graph.nodes().should.be.instanceof(Array)
-      graph.nodes().should.have.length(8)
+      graph.nodes().should.have.length(3)
     })
   })  
 
   describe('#elements', () => {
     it('should return all the elements', () => {
+      const graph = new Graph([map.a, map.b, map.c, map.ac, map.bc])
       graph.elements().should.be.instanceof(Array)
-      graph.elements().should.haveTheSameItemsAs(elements)
+      graph.elements().should.have.length(5)
     })
 
     it('should reproduce the same elements from itself', () => {
+      const elements = [map.a, map.b, map.c, map.ac, map.bc]
+      const graph = new Graph(elements)
       new Graph(graph.elements()).elements().should.haveTheSameItemsAs(elements)
     })
   })
 
   describe('#parents', () => {
     it('should find all the parents of a node', () => {
+      const graph = new Graph([map.a, map.b, map.c, map.d, map.ac, map.bc, map.cd])
       graph.parents('d').should.be.instanceof(Array)
-      graph.parents('d').should.have.length(3)
-      graph.parents('d')[0].should.be.instanceof(Object)
+      graph.parents('d').should.have.length(1)
+      graph.parents('d')[0].should.be.equal(map.c)
     })
   })  
 
   describe('#children', () => {
     it('should find all the children of a node id', () => {
+      const graph = new Graph([map.a, map.b, map.c, map.d, map.ac, map.bc, map.cd])
       graph.children('b').should.be.instanceof(Array)
-      graph.children('b').should.have.length(2)
-      graph.children('b')[0].should.be.instanceof(Object)
+      graph.children('b').should.have.length(1)
+      graph.children('b')[0].should.be.equal(map.c)
     })
 
     it('should find all the children of a node', () => {
