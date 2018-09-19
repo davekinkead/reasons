ec1b67bef390ad9f17bd8147e314c3f89b36124b
Dave Kinkead <dave@kinkead.com.au>
2018-09-19 11:39:55 +0200

    Use keymap library

--- reasons.js
+++ reasons.js
@@ -581,6 +581,7 @@ module.exports = {
 const View    = require('./view')
 const Utils   = require('./utils')
 const Graph   = require('./graph')
+const Keycode = require('keycode')
 const History = []
 let   Future  = []
 
@@ -693,14 +694,14 @@ function addEventListeners (argumentMap) {
 
     if (argumentMap.editMode) {
       //  Escape key
-      if (event.keyCode == 27) removeOverlay(argumentMap)
+      if (Keycode.isEventKey(event, 'escape')) removeOverlay(argumentMap)
 
       //  Return key
-      if (event.keyCode == 13) submitOverlay(argumentMap)
+      if (Keycode.isEventKey(event, 'enter')) submitOverlay(argumentMap)
 
     } else {
       //  Focus on `Tab`
-      if (!event.metaKey && event.keyCode == 9) {
+      if (!event.metaKey && Keycode.isEventKey(event, 'tab')) {
         event.preventDefault()
         selected = argumentMap.graph[0]
         argumentMap.graph.focus(selected)
@@ -708,7 +709,9 @@ function addEventListeners (argumentMap) {
       }
 
       //  Undo `⌘-z`
-      if (event.metaKey && event.keyCode == 90) {
+      if (
+        (event.metaKey || Keycode.isEventKey(event, 'control') || Keycode.isEventKey(event, 'command'))
+         && Keycode.isEventKey(event, 'z')) {
 
         //  Store for redo
         save(Future, argumentMap)
@@ -721,7 +724,9 @@ function addEventListeners (argumentMap) {
       }
 
       //  Redo `⌘-y`
-      if (event.metaKey && event.keyCode == 89) {
+      if (
+        (event.metaKey || Keycode.isEventKey(event, 'control') || Keycode.isEventKey(event, 'command'))
+         && Keycode.isEventKey(event, 'y')) {
 
         //  Store for undo
         save(History, argumentMap)
@@ -734,12 +739,12 @@ function addEventListeners (argumentMap) {
       }
 
       //  Edit selected element on `enter`
-      if (selected && event.keyCode == 13) {
+      if (selected && Keycode.isEventKey(event, 'enter')) {
         addOverlay(argumentMap, selected)
       }
 
       //  Delete a selected element on `backspace` or `delete`
-      if (selected && (event.keyCode == 8 || event.keyCode == 46)) {
+      if (selected && (Keycode.isEventKey(event, 'delete') || Keycode.isEventKey(event, 'backspace'))) {
         event.preventDefault()
         argumentMap.graph.remove(selected)
         argumentMap.dirty = true
@@ -776,7 +781,9 @@ function redraw (argumentMap) {
 }
 
 
-//  Save a serialized copy of the graph
+/**
+ * Private: Saves a serialized copy of the graph
+ */
 function save (store, argumentMap) {
   const last = (store.length == 0) ? JSON.stringify([]) : store[store.length-1]
   const current = JSON.stringify(
@@ -809,6 +816,7 @@ function getPosition (event) {
     y: parseInt(event.y || event.clientY)
   }
 }
+
 
 /**
  * Private: Overlays a text box to edit a node or edge
@@ -860,7 +868,7 @@ function removeOverlay (argumentMap) {
   argumentMap.altered = true
   document.querySelector('#reason-overlay').remove()  
 }
-},{"./graph":4,"./utils":9,"./view":10}],9:[function(require,module,exports){
+},{"./graph":4,"./utils":9,"./view":10,"keycode":14}],9:[function(require,module,exports){
 arguments[4][2][0].apply(exports,arguments)
 },{"array-difference":11,"array-flatten":12,"array-unique":13,"dup":2}],10:[function(require,module,exports){
 'use strict'
@@ -1349,6 +1357,183 @@ module.exports.immutable = function uniqueImmutable(arr) {
 
   return module.exports(newArr);
 };
+
+},{}],14:[function(require,module,exports){
+// Source: http://jsfiddle.net/vWx8V/
+// http://stackoverflow.com/questions/5603195/full-list-of-javascript-keycodes
+
+/**
+ * Conenience method returns corresponding value for given keyName or keyCode.
+ *
+ * @param {Mixed} keyCode {Number} or keyName {String}
+ * @return {Mixed}
+ * @api public
+ */
+
+function keyCode(searchInput) {
+  // Keyboard Events
+  if (searchInput && 'object' === typeof searchInput) {
+    var hasKeyCode = searchInput.which || searchInput.keyCode || searchInput.charCode
+    if (hasKeyCode) searchInput = hasKeyCode
+  }
+
+  // Numbers
+  if ('number' === typeof searchInput) return names[searchInput]
+
+  // Everything else (cast to string)
+  var search = String(searchInput)
+
+  // check codes
+  var foundNamedKey = codes[search.toLowerCase()]
+  if (foundNamedKey) return foundNamedKey
+
+  // check aliases
+  var foundNamedKey = aliases[search.toLowerCase()]
+  if (foundNamedKey) return foundNamedKey
+
+  // weird character?
+  if (search.length === 1) return search.charCodeAt(0)
+
+  return undefined
+}
+
+/**
+ * Compares a keyboard event with a given keyCode or keyName.
+ *
+ * @param {Event} event Keyboard event that should be tested
+ * @param {Mixed} keyCode {Number} or keyName {String}
+ * @return {Boolean}
+ * @api public
+ */
+keyCode.isEventKey = function isEventKey(event, nameOrCode) {
+  if (event && 'object' === typeof event) {
+    var keyCode = event.which || event.keyCode || event.charCode
+    if (keyCode === null || keyCode === undefined) { return false; }
+    if (typeof nameOrCode === 'string') {
+      // check codes
+      var foundNamedKey = codes[nameOrCode.toLowerCase()]
+      if (foundNamedKey) { return foundNamedKey === keyCode; }
+    
+      // check aliases
+      var foundNamedKey = aliases[nameOrCode.toLowerCase()]
+      if (foundNamedKey) { return foundNamedKey === keyCode; }
+    } else if (typeof nameOrCode === 'number') {
+      return nameOrCode === keyCode;
+    }
+    return false;
+  }
+}
+
+exports = module.exports = keyCode;
+
+/**
+ * Get by name
+ *
+ *   exports.code['enter'] // => 13
+ */
+
+var codes = exports.code = exports.codes = {
+  'backspace': 8,
+  'tab': 9,
+  'enter': 13,
+  'shift': 16,
+  'ctrl': 17,
+  'alt': 18,
+  'pause/break': 19,
+  'caps lock': 20,
+  'esc': 27,
+  'space': 32,
+  'page up': 33,
+  'page down': 34,
+  'end': 35,
+  'home': 36,
+  'left': 37,
+  'up': 38,
+  'right': 39,
+  'down': 40,
+  'insert': 45,
+  'delete': 46,
+  'command': 91,
+  'left command': 91,
+  'right command': 93,
+  'numpad *': 106,
+  'numpad +': 107,
+  'numpad -': 109,
+  'numpad .': 110,
+  'numpad /': 111,
+  'num lock': 144,
+  'scroll lock': 145,
+  'my computer': 182,
+  'my calculator': 183,
+  ';': 186,
+  '=': 187,
+  ',': 188,
+  '-': 189,
+  '.': 190,
+  '/': 191,
+  '`': 192,
+  '[': 219,
+  '\\': 220,
+  ']': 221,
+  "'": 222
+}
+
+// Helper aliases
+
+var aliases = exports.aliases = {
+  'windows': 91,
+  '⇧': 16,
+  '⌥': 18,
+  '⌃': 17,
+  '⌘': 91,
+  'ctl': 17,
+  'control': 17,
+  'option': 18,
+  'pause': 19,
+  'break': 19,
+  'caps': 20,
+  'return': 13,
+  'escape': 27,
+  'spc': 32,
+  'spacebar': 32,
+  'pgup': 33,
+  'pgdn': 34,
+  'ins': 45,
+  'del': 46,
+  'cmd': 91
+}
+
+/*!
+ * Programatically add the following
+ */
+
+// lower case chars
+for (i = 97; i < 123; i++) codes[String.fromCharCode(i)] = i - 32
+
+// numbers
+for (var i = 48; i < 58; i++) codes[i - 48] = i
+
+// function keys
+for (i = 1; i < 13; i++) codes['f'+i] = i + 111
+
+// numpad keys
+for (i = 0; i < 10; i++) codes['numpad '+i] = i + 96
+
+/**
+ * Get by code
+ *
+ *   exports.name[13] // => 'Enter'
+ */
+
+var names = exports.names = exports.title = {} // title for backward compat
+
+// Create reverse mapping
+for (i in codes) names[codes[i]] = i
+
+// Add aliases
+for (var alias in aliases) {
+  codes[alias] = aliases[alias]
+}
 
 },{}]},{},[7])(7)
 });
