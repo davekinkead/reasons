33177bdc3031758e47d1b1b7eab5ab1ddd1f8b36
Dave Kinkead <dave@kinkead.com.au>
2019-06-27 09:41:56 +1000

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git checkout master
+git pull github master
+npm run minify
+git commit -am "Autobuild and minify"
+git push github master
+git checkout gh-pages
+git merge master
+git push github gh-pages
+git checkout master
